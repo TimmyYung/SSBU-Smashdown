acbea3cb6fe923fbe8e8e6c6fcd139d421b5edba
TimmyYung <43042577+TimmyYung@users.noreply.github.com>
2020-05-05 14:40:43 -0400

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,385 @@
+$(document).ready(function(){
+  $('#box0').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box1').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box2').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box3').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box4').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box5').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box6').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box7').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box8').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box9').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box10').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box11').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box12').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box13').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box14').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box15').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box16').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box17').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box18').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box19').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box20').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box21').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box22').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box23').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box24').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box25').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box26').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box27').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box28').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box29').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box30').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box31').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box32').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box33').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box34').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box35').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box36').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box37').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box38').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box39').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box40').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box41').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box42').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box43').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box44').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box45').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box46').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box47').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box48').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box49').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box50').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box51').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box52').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box53').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box54').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box55').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box56').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box57').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box58').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box59').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box60').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box61').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box62').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box63').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box64').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box65').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box66').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box67').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box68').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box69').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box70').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box71').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box72').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box73').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box74').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box75').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
+$(document).ready(function(){
+  $('#box76').click(function(){
+      $(this).css('background-color', 'black');
+  });
+});
