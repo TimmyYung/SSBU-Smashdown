3a4af584b19f7aba45f47aaf8433d16dc3eaa557
TimmyYung <43042577+TimmyYung@users.noreply.github.com>
2020-05-05 23:22:29 -0400

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -7,7 +7,7 @@ for (const box of boxes) {
 }
 
 function addSpan() {
-  for (let i = 0; i < 76; i++) {
+  for (let i = 0; i < 77; i++) {
     let span = document.createElement("span");
     span.addEventListener("click", () => {
       span.style.background = "black";
